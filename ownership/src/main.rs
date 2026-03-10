2cd734c9ecb2dba0ce30b8ba90639fff3bfd4615
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2026-03-10 15:40:49 -0600

    Add ownership project

---
+++ ownership/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Mini proyecto 1
+    let s1 = String::from("Hola");
+    let s2 = s1;
+    println!("{}", s2);
+    //println!("{}", s1);
+
+    // Mini proyecto 2
+    let a = 5;
+    let b = a;
+    println!("{}", a);
+    println!("{}", b);
+}
