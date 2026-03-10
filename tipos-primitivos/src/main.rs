8dfd92a0b4a3eb5a23b0f12c9db56a318c579143
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2026-03-09 20:16:04 -0600

    Add Primitive types project

---
+++ tipos-primitivos/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  // Mini proyecto 1
+  let width:isize = 10;
+  let height = 5;
+  let area = width * height;
+  println!("El área es: {}", area);
+  // Mini proyecto 2
+  let entero: i32 = 10;
+  let entero = entero as f64;
+  let cociente = entero / 4.0;
+  println!("Resultado: {}", cociente);
+}
